8f16d9b3c4313c377e9f3d31671fce77573bb642
Brian C. Winkler <bcw9368@rit.edu>
2012-05-11 20:58:12 -0400

    Added in 2-step parallel annealer. Accuracy is maintained for much smaller number of initial starting points.

---
+++ src/Parallel2StepAnnealer.java
@@ -0,0 +1,96 @@
+import edu.rit.pj.Comm;
+import edu.rit.util.Random;
+
+import edu.rit.pj.IntegerForLoop;
+import edu.rit.pj.ParallelSection;
+import edu.rit.pj.ParallelRegion;
+import edu.rit.pj.ParallelTeam;
+import edu.rit.pj.reduction.SharedDouble;
+import edu.rit.pj.reduction.SharedDoubleArray;
+
+public class Parallel2StepAnnealer extends AbstractAnnealer{
+
+  public Parallel2StepAnnealer(  
+      final int numPoints,
+      final int dim,
+      final double[] lb,
+      final double[] ub,
+      final Function f,
+      final int numThreads ){
+    super( numPoints, dim, lb, ub, f );
+  }
+  public Parallel2StepAnnealer(  
+      final int numPoints,
+      final int dim,
+      final double[] lb,
+      final double[] ub,
+      final Function f ){
+    super( numPoints, dim, lb, ub, f );
+  }
+
+  public void run(){
+
+    try{
+      new ParallelTeam( ).execute( new ParallelRegion(){
+        public void run() throws Exception{
+          execute(0, numPoints - 1, new IntegerForLoop(){
+            double myfOpt = fOpt;
+            double[] myxOpt = new double[ xOpt.length ];
+            public void run( int first, int last ) throws Exception{
+
+              for( int i = first; i <= last; i++ ){
+                double[] x0 = new double[ dim ];
+                for( int j = 0; j < dim; j++ ){
+                  x0[j] = boundedRandom( lb[j], ub[j] );
+                }
+                ObjectiveFunction obj 
+                  = new ObjectiveFunction( x0, lb, ub, f );
+                SimulatedAnnealer sa 
+                  = AnnealerFactory.getAnnealer( dim, lb, ub, x0, obj );
+                sa.run();
+                xOptHist[i] = sa.getSolution();
+                fOptHist[i] = sa.getValue();
+                if( fOptHist[i] < myfOpt ){
+                  myfOpt = fOptHist[i];
+                  myxOpt = xOptHist[i];
+                }
+              }
+              critical( new ParallelSection(){
+                public void run(){
+                  if( myfOpt < fOpt ){
+                    fOpt = myfOpt;
+                    xOpt = myxOpt;
+                  }
+                }
+              } );
+
+              ObjectiveFunction obj 
+                = new ObjectiveFunction( xOpt, lb, ub, f );
+              SimulatedAnnealer sa 
+                = AnnealerFactory.getAnnealer( dim, lb, ub, xOpt, obj );
+              sa.run();
+              myxOpt = sa.getSolution();
+              myfOpt = sa.getValue();
+            }
+            public void finish() throws Exception{ 
+              critical( new ParallelSection(){
+                public void run(){
+                  if( myfOpt < fOpt ){
+                    fOpt = myfOpt;
+                    xOpt = myxOpt;
+                  }
+                }
+              } );
+
+            }
+          } );
+        }
+      } );
+    } 
+    catch( Exception e ){
+
+    }
+
+  } 
+
+}
